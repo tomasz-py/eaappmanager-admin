7bd523f245d35a2154d39a03277aba10b9fde971
Tomasz-py <tomasz.pyclik@gmail.com>
2019-03-07 11:56:31 +0100

    Test CRUD on Blogs model

--- src/App.js
+++ src/App.js
@@ -1,53 +1,32 @@
-import React, { Component } from "react";
+import React from "react";
 import { Admin, Resource } from "react-admin";
-import { UserList } from "./users/users";
-import { PostList, PostEdit, PostCreate, BlogList } from "./posts/posts";
-import jsonServerProvider from "ra-data-json-server";
+//import { UserList } from "./users/users";
+//import { PostList, PostEdit, PostCreate, BlogList } from "./posts/posts";
+//import jsonServerProvider from "ra-data-json-server";
 import Dashboard from "./Dashboard/Dashboard";
-import PostIcon from "@material-ui/icons/Book";
+//import PostIcon from "@material-ui/icons/Book";
 import UserIcon from "@material-ui/icons/Group";
 import authProvider from "./authProvider/authProvider";
 import loopbackClient from "react-admin-loopback";
-
-import { List, Datagrid, TextField } from "react-admin";
+import { BlogsList, BlogEdit, BlogCreate } from "./Blogs/Blogs";
 
 //const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
-const Blist = props => (
-  <List {...props}>
-    <Datagrid>
-      <TextField source="id" />
-      <TextField source="title" />
-      <TextField source="author" />
-      <TextField source="creationdate" />
-    </Datagrid>
-  </List>
-);
+const dataProvider = loopbackClient("http://localhost:3000/api/");
 
 const App = () => (
   <Admin
     dashboard={Dashboard}
-    dataProvider={loopbackClient("http://localhost:3000/api/")}
+    dataProvider={dataProvider}
     authProvider={authProvider}
   >
-    <Resource name="blogs" list={Blist} icon={UserIcon} />
+    <Resource
+      name="blogs"
+      list={BlogsList}
+      icon={UserIcon}
+      create={BlogCreate}
+      edit={BlogEdit}
+    />
   </Admin>
 );
-
-// const App = () => (
-//   <Admin
-//     dashboard={Dashboard}
-//     dataProvider={dataProvider}
-//     authProvider={authProvider}
-//   >
-//     <Resource name="users" list={UserList} icon={UserIcon} />
-//     <Resource
-//       name="posts"
-//       list={PostList}
-//       edit={PostEdit}
-//       create={PostCreate}
-//       icon={PostIcon}
-//     />
-//   </Admin>
-// );
 
 export default App;
