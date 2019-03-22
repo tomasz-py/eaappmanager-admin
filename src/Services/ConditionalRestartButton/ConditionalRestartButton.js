d0c39f59a25e7b8688320f3104e4efefced7bbfe
Tomasz-py <tomasz.pyclik@gmail.com>
2019-03-22 22:48:21 +0100

    Add restart btn with page refresh

--- src/Services/ConditionalRestartButton/ConditionalRestartButton.js
+++ src/Services/ConditionalRestartButton/ConditionalRestartButton.js
@@ -1,13 +1,34 @@
 import React from "react";
-import { Button } from "react-admin";
+import { Button, RefreshButton } from "react-admin";
 import RefreshIcon from "@material-ui/icons/Refresh";
+import { dataProvider } from "../../App";
+import { CREATE, UPDATE } from "react-admin";
+// import { Link } from "react-router-dom";
+// import { push as pushAction } from "react-router-redux";
 
-const ConditionalRestartButton = ({ record, ...rest }) => {
+const ConditionalRestartButton = ({ record, resource, ...rest }) => {
   const onClickHandler = () => {
-    console.log(rest);
+    dataProvider(CREATE, "queues", {
+      data: {
+        tableName: "service",
+        itemId: record.id,
+        statusId: 3,
+        action: "Restart"
+      }
+    })
+      .then(
+        dataProvider(UPDATE, "services", {
+          id: record.id,
+          data: {
+            statusId: 5
+          }
+        })
+      )
+      .then(() => window.location.reload())
+      .catch(e => console.log(e));
   };
 
-  return record.toRestart ? (
+  return record.toRestart && record.statusId !== 5 ? (
     <Button label="Restart" onClick={onClickHandler}>
       <RefreshIcon />
     </Button>
@@ -15,4 +36,8 @@ const ConditionalRestartButton = ({ record, ...rest }) => {
 };
 export default ConditionalRestartButton;
 
-//record.toRestart ? <EditButton record={record} {...rest} /> : null;
+// <RefreshButton
+//   resource={resource}
+//   label="Restart"
+//   //onClick={onClickHandler}
+// />
