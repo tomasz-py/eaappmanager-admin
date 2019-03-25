502d5481bc7c372758e3c07b7c7aa5f9d5c6d428
Tomasz-py <tomasz.pyclik@gmail.com>
2019-03-25 19:08:25 +0100

    Add expansion panel to server list /dashboard

--- src/Dashboard/Content/DBServers.js
+++ src/Dashboard/Content/DBServers.js
@@ -10,6 +10,12 @@ import Paper from "@material-ui/core/Paper";
 import { GET_LIST } from "react-admin";
 import { dataProvider } from "../../App";
 
+import ExpansionPanel from "@material-ui/core/ExpansionPanel";
+import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import Typography from "@material-ui/core/Typography";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+
 const styles = theme => ({
   root: {
     width: "100%",
@@ -18,6 +24,11 @@ const styles = theme => ({
   },
   table: {
     minWidth: 700
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular,
+    backgroundColor: "#d3d3d3"
   }
 });
 
@@ -61,28 +72,46 @@ const DBServers = props => {
 
   return (
     <Paper className={classes.root}>
-      <Table className={classes.table}>
-        <TableHead>
-          <TableRow>
-            <TableCell>Name</TableCell>
-            <TableCell align="right">Host</TableCell>
-            <TableCell align="right">IP</TableCell>
-            <TableCell align="right">Status</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {servers.map(server => (
-            <TableRow key={server.id}>
-              <TableCell component="th" scope="row">
-                {server.name}
-              </TableCell>
-              <TableCell align="right">{server.host}</TableCell>
-              <TableCell align="right">{server.ip}</TableCell>
-              <TableCell align="right">{server.status.name}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+      {servers.map(server => (
+        <ExpansionPanel key={server.id}>
+          <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography className={classes.heading}>
+              {"Name: " +
+                server.name +
+                " || Host: " +
+                server.host +
+                " || IP: " +
+                server.ip +
+                " || Status: " +
+                server.status.name}
+            </Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Table className={classes.table}>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Name</TableCell>
+                  <TableCell align="right">Host</TableCell>
+                  <TableCell align="right">IP</TableCell>
+                  <TableCell align="right">Status</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {server.service.map(s => (
+                  <TableRow key={s.id}>
+                    <TableCell component="th" scope="row">
+                      {s.servicetype.name}
+                    </TableCell>
+                    <TableCell align="right">{s.port}</TableCell>
+                    <TableCell align="right">{s.description}</TableCell>
+                    <TableCell align="right">{s.status.name}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+      ))}
     </Paper>
   );
 };
